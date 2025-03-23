46809d7c4ce390ef2eae44342f068dad364be945
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-23 17:54:56 +0100

    Páginas de home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,8 +11,49 @@ import { UserCardComponent } from '../../shared/user-card/user-card.component';
 })
 export class HomeComponent {
   arrUsers: IUser[] = [];
+  page = 1;
+  totalPages = 2
   UsersService = inject(UserService);
 
+  
+    ngOnInit(): void {
+      this.loadUsers();
+    }
+  
+    async loadUsers() {
+      try {
+        const resp = await this.UsersService.getAllPromise(this.page);
+        this.arrUsers = resp.results;
+        this.page = resp.page;
+        this.totalPages = resp.total_pages;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  
+    nextPage() {
+      if (this.page < this.totalPages) {
+        this.page++;
+        this.loadUsers();
+      }
+    }
+  
+    prevPage() {
+      if (this.page > 1) {
+        this.page--;
+        this.loadUsers();
+      }
+    }
+
+    goToPage(pageNumber: number) {
+      this.page = pageNumber;
+      this.loadUsers();
+    }
+  }
+  
+  
+  
+/*
   ngOnInit() {
     this.loadUsers()
   }
@@ -32,6 +73,6 @@ export class HomeComponent {
       //refrescar la lista de empleados this.arrEmployees
       this.loadUsers()
     }
-  }
+  }*/
 
-}
+
