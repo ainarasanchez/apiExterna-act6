7f551b5af36eb0e5e1c367ff48b12ca6b69573af
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-23 16:02:02 +0100

    Importación ngx sonner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
+import { toast, NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
